dd62f36eec7867e386785de182b60eeb0b33c2bd
Chris Sewell <chrisj_sewell@hotmail.com>
2020-02-13 11:30:14 +1100

    Allow block tokenization to track line numbers
    This will allow block tokens to store the start/end line range which they relate to in the source text.
    If a block performs a 'sub-tokenization', it should call `tokenize_block` with a new `start_line`

--- mistletoe/block_tokenizer.py
+++ mistletoe/block_tokenizer.py
@@ -4,10 +4,15 @@ Block-level tokenizer for mistletoe.
 
 
 class FileWrapper:
-    def __init__(self, lines):
+    def __init__(self, lines, start_line=0):
         self.lines = lines if isinstance(lines, list) else list(lines)
         self._index = -1
         self._anchor = 0
+        self.start_line = start_line
+
+    @property
+    def lineno(self):
+        return self.start_line + self._index
 
     def __next__(self):
         if self._index + 1 < len(self.lines):
@@ -19,7 +24,7 @@ class FileWrapper:
         return self
 
     def __repr__(self):
-        return repr(self.lines[self._index+1:])
+        return repr(self.lines[self._index + 1 :])
 
     def anchor(self):
         self._anchor = self._index
@@ -29,7 +34,7 @@ class FileWrapper:
 
     def peek(self):
         if self._index + 1 < len(self.lines):
-            return self.lines[self._index+1]
+            return self.lines[self._index + 1]
         return None
 
     def backstep(self):
@@ -51,14 +56,14 @@ def tokenize(iterable, token_types):
     return make_tokens(tokenize_block(iterable, token_types))
 
 
-def tokenize_block(iterable, token_types):
+def tokenize_block(iterable, token_types, start_line=0):
     """
     Returns a list of pairs (token_type, read_result).
 
     Footnotes are parsed here, but span-level parsing has not
     started yet.
     """
-    lines = FileWrapper(iterable)
+    lines = FileWrapper(iterable, start_line)
     parse_buffer = ParseBuffer()
     line = lines.peek()
     while line is not None:
@@ -96,6 +101,7 @@ class ParseBuffer(list):
     A wrapper around builtin list,
     so that setattr(list, 'loose') is legal.
     """
+
     def __init__(self, *args):
         super().__init__(*args)
         self.loose = False
