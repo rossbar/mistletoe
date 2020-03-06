a42305157c0627484752a3e08ddecc4a47aac77d
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-06 00:21:01 +0000

    Replace `test_ci.sh` by file_regression tests

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     python_requires="~=3.4",
     extras_require={
         "code_style": ["flake8<3.8.0,>=3.7.0", "black==19.10b0", "pre-commit==1.17.0"],
-        "testing": ["coverage", "pytest>=3.6,<4", "pytest-cov"],
+        "testing": ["coverage", "pytest>=3.6,<4", "pytest-cov", "pytest-regressions"],
     },
     zip_safe=False,
 )
